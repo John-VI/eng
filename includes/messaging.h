3bf71e08eeb6dd8e9d675978867ed56c93b5edd7
John-VI <dogjaw2233@protonmail.com>
2021-08-12 13:04:15 -0400

    Movin to my laptop

--- includes/messaging.h
+++ includes/messaging.h
@@ -7,11 +7,12 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
 
-enum MSGLEVEL { DEBUG, INFO, MESSAGE, WARN, ERR, CRITICAL };
+enum MSGLEVEL { DEBUG, INFO, MESSAGE, WARN, ERR, CRITICAL, FATAL, LEVELS };
 
 struct message {
   std::string msgtext;
   MSGLEVEL level;
+  SDL_Color *altcolor = NULL;
 };
   
 template <class T>
@@ -22,16 +23,19 @@ void invert(T *sequence, unsigned int size);
 
 class messager {
 public:
-  messager(std::shared_ptr<spdlog::logger> log, uint16_t flag, unsigned int size);
+  messager(std::shared_ptr<spdlog::logger> log, unsigned int size, font *fontface, );
   void send(std::string message, MSGLEVEL level);
   void send(struct message message);
   void rendermessages(unsigned int from, int x, int y, unsigned int width, unsigned int height);
   uint16_t flags = 0;
+  MSGLEVEL level = DEBUG;
 protected:
   std::shared_ptr<spdlog::logger> logger;
   std::string *queue;
   unsigned int queuesize;
   font *face;
+  SDL_Color *palette;
+  unsigned int index[LEVELS];
 };
 
 #endif
